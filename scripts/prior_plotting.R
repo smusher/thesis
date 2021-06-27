6dc063ea038ce0a1ac570778d6816897fbdf8fef
sam <samuel.g.usher@gmail.com>
2021-06-27 18:53:02 +0100

    started bayesian methods

--- scripts/prior_plotting.R
+++ scripts/prior_plotting.R
@@ -18,3 +18,84 @@ Ka <- dist_normal(log(1e4), log(5))
 sigma <- dist_cauchy(0, 1)
 
 (autoplot(L) + autoplot(D)) / (autoplot(Ka) + autoplot(sigma))
+
+#fair coin
+fair_flips <- rbinom(10, 1, 0.5)
+
+#unfair coin
+unfair_flips <- rbinom(10, 1, 0.8)
+
+fair_toss <-
+	tibble(
+		n_trials = c(10, 50, 100),
+		) %>%
+	rowwise() %>%
+	mutate(
+		n_success = rbinom(1, n_trials, 0.5)
+		) %>%
+	expand(
+		nesting(n_trials, n_success), p_heads = seq(from = 0, to = 1, length.out = 51)
+		) %>%
+	group_by(n_trials) %>%
+	mutate(
+		prior = dunif(p_heads, 0, 1),
+        likelihood = dbinom(
+        	x = n_success,
+        	size = n_trials,
+        	prob = p_heads
+        	),
+        posterior = prior * likelihood
+        ) %>%
+  	mutate(
+  		prior = prior / sum(prior),
+        likelihood = likelihood / sum(likelihood),
+        posterior = posterior / sum(posterior)
+        ) %>%
+  	pivot_longer(c(prior, likelihood, posterior))
+
+ggplot(fair_toss, aes(x = p_heads)) +
+geom_line(aes(y = value, colour = name)) +
+scale_x_continuous("proportion heads", breaks = c(0, .5, 1)) +
+scale_y_continuous("plausibility", breaks = NULL) +
+theme(panel.grid = element_blank()) +
+facet_grid(rows = vars(name), cols = vars(n_trials))
+
+unfair_toss <-
+	tibble(
+		n_trials = c(10, 100, 1000),
+		n_success_fair = c(5, 50, 500)
+		) %>%
+	rowwise() %>%
+	mutate(
+		n_success = rbinom(1, n_trials, 0.8)
+		) %>%
+	expand(
+		nesting(n_trials, n_success, n_success_fair), p_heads = seq(from = 0, to = 1, length.out = 51)
+		) %>%
+	group_by(n_trials) %>%
+	mutate(
+		prior = dbinom(
+        	x = 5,
+        	size = 10,
+        	prob = p_heads
+        	),
+        likelihood = dbinom(
+        	x = n_success,
+        	size = n_trials,
+        	prob = p_heads
+        	),
+        posterior = prior * likelihood
+        ) %>%
+  	mutate(
+  		prior = prior / sum(prior),
+        likelihood = likelihood / sum(likelihood),
+        posterior = posterior / sum(posterior)
+        ) %>%
+  	pivot_longer(c(prior, likelihood, posterior))
+
+ggplot(unfair_toss, aes(x = p_heads)) +
+geom_line(aes(y = value, colour = name)) +
+scale_x_continuous("proportion heads", breaks = c(0, .5, 1)) +
+scale_y_continuous("plausibility", breaks = NULL) +
+theme(panel.grid = element_blank()) +
+facet_wrap(~n_trials, scales = "free")
